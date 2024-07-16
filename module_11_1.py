b86fa948155301ce1ef236ac34941fa54fcff189
MisterCbing <da@susanin.news>
2024-07-16 22:09:33 +0400

    v 0.1

--- module_11_1.py
+++ module_11_1.py
@@ -1,3 +1,20 @@
+import math
+def introspection_info(obj):
+    import inspect
+    info = {}
+    info['type'] = str(type(obj)).split("'")[1]
+    info['module'] = inspect.getmodule(obj)
+    print(inspect.getmodule(obj))
+    # for i in dir(obj):
+    #     attr = getattr(obj, i)
+    #     print(i, type(attr)))
 
+    return info
+a = []
+def function(x):
+    return x**2
 
+c = 42
+func = function
 
+print(introspection_info(math.pi))
