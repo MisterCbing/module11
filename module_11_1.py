7c29f1d740309b90df767625c095379e6d56550d
MisterCbing <da@susanin.news>
2024-07-17 17:36:28 +0400

    v 1.0

--- module_11_1.py
+++ module_11_1.py
@@ -1,20 +1,45 @@
-import math
 def introspection_info(obj):
     import inspect
     info = {}
-    info['type'] = str(type(obj)).split("'")[1]
+    if inspect.isclass(obj):
+        info['type'] = 'class'
+    else:
+        info['type'] = str(type(obj)).split("'")[1]
+    info['attributes'] = [attr for attr in dir(obj) if not callable(getattr(obj, attr))]
+    info['methods'] = [method for method in dir(obj) if callable(getattr(obj, method))]
     info['module'] = inspect.getmodule(obj)
-    print(inspect.getmodule(obj))
-    # for i in dir(obj):
-    #     attr = getattr(obj, i)
-    #     print(i, type(attr)))
-
     return info
+
+
+class Sclass:
+    attr1 = 42
+    attr2 = 3.14
+    attr3 = []
+
+    def __init__(self):
+        pass
+
+    def ninja(self):
+        pass
+
+    def samurai(self):
+        pass
+
+    def ronin(self):
+        pass
+
+
 a = []
+
+
 def function(x):
-    return x**2
+    return x ** 2
+
 
 c = 42
 func = function
 
-print(introspection_info(math.pi))
+print(introspection_info(Sclass))
+print(introspection_info(a))
+print(introspection_info(func))
+print(introspection_info(c))
